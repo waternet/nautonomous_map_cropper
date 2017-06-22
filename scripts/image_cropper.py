742423b17e6d76fa037871c2e62963746f1591ee
Daan Zeeuwe <daan_zeeuwe@hotmail.com>
2017-06-22 09:23:25 +0200

    Added new map and removed canal segments.

--- scripts/image_cropper.py
+++ scripts/image_cropper.py
@@ -11,11 +11,12 @@ from geometry_msgs.msg import Pose2D
 from passive_map_cropper_node import publish_initial_position
 
 original_image = None
+image_name = "amsterdam"
 
 # Load the entire image of amsterdam so we can use it to crop it.
 def load_full_image():
     global original_image
-    full_image_string = project_path + "/images/amsterdam_total.png"
+    full_image_string = project_path + "/images/"+ str(image_name) + ".png"
     original_image = Image.open(full_image_string)
 
 # def crop_map_edge(edge):
@@ -65,7 +66,7 @@ def crop_map_points(points,	name_map):
 
 # Open the config file from the original image
 def open_config_file():
-	with open(project_path + "/config/amsterdam_total.yaml") as f:
+	with open(project_path + "/config/"+ str(image_name) + ".yaml") as f:
 		map_data = yaml.safe_load(f)
 	return map_data
 
@@ -152,7 +153,7 @@ def save_cropped_image(original_image, file_name, resolution, left_position, rig
 			int(bottom_position / resolution)
 		)
 	)
-	cropped_example.save(project_path + "/images/amsterdam_cropped_" + str(file_name) + ".png")
+	cropped_example.save(project_path + "/images/"+ str(image_name) + "_cropped_" + str(file_name) + ".png")
 
 # Save config file based on the cropped positions
 def save_config_file(file_name, map_data, left_position, right_position, bottom_position, top_position):
@@ -166,12 +167,12 @@ def save_config_file(file_name, map_data, left_position, right_position, bottom_
 	map_data["map_right"] = right_position
 	map_data["map_bottom"] = bottom_position
 	map_data["map_top"] = top_position
-	map_data["image"] = "/images/amsterdam_cropped_" + str(file_name) + ".png"
+	map_data["image"] = "/images/"+ str(image_name) + "_cropped_" + str(file_name) + ".png"
 	map_data["origin"] = [left_position+map_left, (map_top - map_bottom) - bottom_position + map_bottom, 0.0]
 
     # Save the image and config name
 	image_file_name = project_path + map_data["image"]
-	config_file_name = project_path + "/config/amsterdam_cropped_" + str(file_name) + ".yaml"
+	config_file_name = project_path + "/config/"+ str(image_name) + "_cropped_" + str(file_name) + ".yaml"
  
     # Open the config file and dump the data
 	with open(config_file_name, "w") as f:
