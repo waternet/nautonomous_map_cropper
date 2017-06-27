e0030ba20aa89ba59291da7b42209bdcac79c681
Daan Zeeuwe <daan_zeeuwe@hotmail.com>
2017-06-27 15:11:00 +0200

    Renamed variables and messages.

--- scripts/image_cropper.py
+++ scripts/image_cropper.py
@@ -1,14 +1,16 @@
 
 import math
 import rospy
-import os
+import rospkg
 
 from PIL import Image
 import yaml
 
 from geometry_msgs.msg import Pose2D
 
-project_path = os.path.abspath(os.path.join(os.path.split(__file__)[0], os.pardir))
+rospack = rospkg.RosPack()
+
+nautonomous_configuration_path = rospack.get_path('nautonomous_configuration')
 
 original_image = None
 image_name = "amsterdam"
@@ -16,7 +18,7 @@ image_name = "amsterdam"
 # Load the entire image of amsterdam so we can use it to crop it.
 def load_full_image():
     global original_image
-    full_image_string = project_path + "/config/map/"+ str(image_name) + ".png"
+    full_image_string = nautonomous_configuration_path + "/config/map/"+ str(image_name) + ".png"
     original_image = Image.open(full_image_string)
 
 # Crop the map using a list of points
@@ -25,17 +27,17 @@ def crop_map_points(points,	name_map):
 
     map_data = open_config_file()
     # extract the position of the cropped map
-    resolution, left_position, right_position, bottom_position, top_position, x, y, theta = extract_cropped_image_positions(map_data, points)
+    resolution, left_position, right_position, bottom_position, top_position = extract_cropped_image_positions(map_data, points)
     # save the cropper image based on the positions
     save_cropped_image(original_image, name_map, resolution, left_position, right_position, bottom_position, top_position)
     # save the config file based on the positions
     image_file_name, config_file_name =  save_config_file(name_map, map_data, left_position, right_position, bottom_position, top_position)
 
-    return image_file_name, config_file_name, x, y, theta
+    return image_file_name, config_file_name
 
 # Open the config file from the original image
 def open_config_file():
-	with open(project_path + "/config/map/"+ str(image_name) + ".yaml") as f:
+	with open(nautonomous_configuration_path + "/config/map/" + str(image_name) + ".yaml") as f:
 		map_data = yaml.safe_load(f)
 	return map_data
 
@@ -65,13 +67,12 @@ def extract_cropped_image_positions(map_data, nodes):
 	margin = 50 # magic 50 meters boundary
 
 	# find rectangle that fits the path
-	print "Nodes: " + str(min(nodes_x)) + " " + str(max(nodes_x)) + " " + str(max(nodes_y)) + " " + str(min(nodes_y))
 	left_position = min(nodes_x) - margin
 	right_position = max(nodes_x) + margin
 	bottom_position = max(nodes_y) + margin
 	top_position = min(nodes_y) - margin
 
-	return resolution, left_position, right_position, bottom_position, top_position, x, y, theta
+	return resolution, left_position, right_position, bottom_position, top_position
 
 # Save the cropped image based on the cropped positions
 def save_cropped_image(original_image, file_name, resolution, left_position, right_position, bottom_position, top_position):
@@ -84,7 +85,7 @@ def save_cropped_image(original_image, file_name, resolution, left_position, rig
 			int(bottom_position / resolution)
 		)
 	)
-	cropped_example.save(project_path + "/config/map/"+ str(image_name) + "_cropped_" + str(file_name) + ".png")
+	cropped_example.save(nautonomous_configuration_path + "/config/map/" + str(image_name) + "_cropped_" + str(file_name) + ".png")
 
 # Save config file based on the cropped positions
 def save_config_file(file_name, map_data, left_position, right_position, bottom_position, top_position):
@@ -102,8 +103,8 @@ def save_config_file(file_name, map_data, left_position, right_position, bottom_
 	map_data["origin"] = [left_position+map_left, (map_top - map_bottom) - bottom_position + map_bottom, 0.0]
 
     # Save the image and config name
-	image_file_name = project_path + map_data["image"]
-	config_file_name = project_path + "/config/map/"+ str(image_name) + "_cropped_" + str(file_name) + ".yaml"
+	image_file_name = nautonomous_configuration_path + "/config/map/" + str(image_name) + "_cropped_" + str(file_name) + ".png"
+	config_file_name = nautonomous_configuration_path + "/config/map/" + str(image_name) + "_cropped_" + str(file_name) + ".yaml"
  
     # Open the config file and dump the data
 	with open(config_file_name, "w") as f:
--- scripts/passive_map_cropper_node.py
+++ scripts/passive_map_cropper_node.py
@@ -4,33 +4,24 @@ from PIL import Image
 import rospy
 
 from geometry_msgs.msg import Pose2D
-from nautonomous_map_msgs.srv import CropMapPoints
+from nautonomous_map_msgs.srv import Crop, CropResponse
 
-import image_cropper
-
-initial_pose_pub = None
-
-def publish_initial_position(x, y, theta):
-	global initial_pose_pub
-	print "Publish initial position: " + str(x) + " " + str(y) + " " + str(theta)
-	initial_pose_pub.publish(Pose2D(x, y, theta))		
+import image_cropper	
 
 # Create the service so other nodes can request to crop the map.
 def crop_map_points_service(request):
-	image_file_name, config_file_name, x, y, theta = image_cropper.crop_map_points(request.pathLocations, request.operation_name)
-	publish_initial_position(x, y, theta)
-	return CropMapPointsResponse(image_file_name, config_file_name)
+	image_name, config_name = image_cropper.crop_map_points(request.route, request.name)
+
+  	return CropResponse(image_name, config_name)
 	
 def main():
 	global initial_pose_pub
-	rospy.init_node('nautonomous_map_cropper')
+	rospy.init_node('passive_map_cropper_node')
 
 	print "Passive map cropper"
 	image_cropper.load_full_image()
 
-	initial_pose_pub = rospy.Publisher('/map/cropper/initial_position', Pose2D, queue_size=10)
-
-	s = rospy.Service('/map/cropper/crop_map_points', CropMapPoints, crop_map_points_service)
+	s = rospy.Service('/map/cropper/crop', Crop, crop_map_points_service)
 
 	rospy.spin()
 
